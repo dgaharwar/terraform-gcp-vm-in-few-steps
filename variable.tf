4279dc6e01c25bcbfeb4ca3a51ae5e5c3298bae1
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2022-03-30 15:02:59 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "gcp_private_key" { 
+  type = string 
+} 
+
+variable "gcp_cred" { 
+  type = map 
+} 
+
+locals {
+  credential = merge(var.gcp_cred, {private_key = "${var.gcp_private_key}"}) 
+}
