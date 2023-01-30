f30429cc94f767e85057a263fc60217de0275369
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2023-01-30 14:43:06 +0000

    Update create-instance.tf

--- create-instance.tf
+++ create-instance.tf
@@ -43,5 +43,5 @@ resource "google_compute_firewall" "http-server" {
 }
 
 output "ip" {
-  value = "google_compute_instance.default.network_interface.0.nat_ip"
+  value = "google_compute_instance.default.network_interface.0.network_ip"
 }
